186197def04fda03a289d3f02374cacb2d1b9a3e
Dosymzhan <do5ymzh4n@gmail.com>
2026-03-13 11:49:53 +0500

    finish payment microservice

---
+++ services/payment/src/main/java/com/senizdegen/ecommerce/payment/PaymentService.java
@@ -0,0 +1,30 @@
+package com.senizdegen.ecommerce.payment;
+
+import com.senizdegen.ecommerce.notification.NotificationProducer;
+import com.senizdegen.ecommerce.notification.PaymentNotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+        var payment = repository.save(mapper.toPayment(request));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
+    }
+}
