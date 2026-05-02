d0439afc3f1b46c99b3420171e7e838815102da9
senizdegen <seisendosymzhan05@gmail.com>
2026-05-02 20:06:32 +0500

    fix migrations/.env

--- services/auth/migrations/env.py
+++ services/auth/migrations/env.py
@@ -62,15 +62,9 @@ def do_run_migrations(connection: Connection) -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {"sqlalchemy.url": Config.DATABASE_URL},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
